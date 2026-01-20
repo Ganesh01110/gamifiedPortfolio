45e68a476a66a0461eed98fa2239eae4d6768797
ganesh sahu <ganeshsahu0108@gmail.com>
2026-01-20 15:25:49 +0530

    feat: implement PWA support and SEO optimizations

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'ganesh-sahu-portfolio-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/globals.css',
+    '/manifest.json',
+    '/favicon.ico',
+    '/assets/gamebackground.png',
+    '/logo2.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+            );
+        })
+    );
+});
