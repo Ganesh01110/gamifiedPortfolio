39b343977e3a9dba729090f5cff91f6be0aed2fd
ganesh sahu <ganeshsahu0108@gmail.com>
2026-01-24 17:50:06 +0530

    fix: resolve circular structure error in eslint flat config for github actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "public/**/*",
-      ".next/**/*",
-      "node_modules/**/*",
-      "dist/**/*",
-      "build/**/*",
-      "*.config.js",
-      "*.config.mjs",
-      "*.config.ts"
-    ],
-  }
-];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "public/**",
+    "dist/**",
+    "node_modules/**",
+    "next-env.d.ts",
+    "*.config.js",
+    "*.config.mjs",
+    "*.config.ts"
+  ]),
+]);
 
 export default eslintConfig;
